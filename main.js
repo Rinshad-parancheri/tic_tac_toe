85ac91365f3846a993e5bfeae7f0a05392f8527e
rinshadpdev <rinshadp014@gmail.com>
2023-11-29 08:21:51 +0530

    refactore the code with help of gpt
    almost all thing have been avoided from the global object and used IEFE to enscapulate , i have learned IEFE and clousre in this and learned to create object without contruction

--- main.js
+++ main.js
@@ -1,187 +1,151 @@
-const { type } = require('os');
-const readline = require('readline');
+const GameModule = (() => {
+    const createGame = (rl) => {
+        let currentPlayer = 'X';
+        const gameBoard = [
+            [1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9]
+        ];
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-const gmae = function () {
-    let currentPlayer = 'X';
-    let gameBoard = [
-        [1, 2, 3],
-        [4, 5, 6],
-        [7, 8, 9]
-    ];
-
-    function displayGameBoard() {
-        for (let i = 0; i < 3; i++) {
-            console.log(gameBoard[i].join(' | '));
-            if (i < 2) {
-                console.log('---------');
-            }
-        }
-    }
-
-
-    function getSelection() {
-        rl.question(`Player ${currentPlayer}, enter the number of your column: `, (input) => {
-            const number = parseInt(input);
-            const [row, col] = findTheGridPosition(number);
-            if (row === 10 || col === 10 || typeof gameBoard[row][col] !== 'number') {
-                console.log('Invalid selection! Enter again.');
-                getSelection(); // Restart selection process
-            } else {
-                updateGameBoard(row, col);
-            }
-        });
-    }
-
-
-
-    function updateGameBoard(row, col) {
-        updateGmaeStatus(row, col);
-        displayGameBoard();
-        if (checkWinner()) {
-            updateWinner();
-        } else if (isBoardFull()) {
-            determineIfGaemIsDraw();
-        } else {
-            changeCurrentPlayer();
-        }
-
-    }
-
-    function updateGmaeStatus(row, col) {
-        gameBoard[row][col] = currentPlayer;
-
-    }
-
-    function changeCurrentPlayer() {
-        currentPlayer = (currentPlayer === 'X') ? 'O' : 'X';
-        getSelection();
-    }
-
-    function determineIfGaemIsDraw() {
-
-        console.log('It\'s a draw!');
-        displayGameBoard();
-        rl.close();
-
-    }
-
-    function isBoardFull() {
-        for (let i = 0; i < 3; i++) {
-            for (let j = 0; j < 3; j++) {
-                if (typeof gameBoard[i][j] === 'number') {
-                    return false;
+        const displayGameBoard = () => {
+            for (let i = 0; i < 3; i++) {
+                console.log(gameBoard[i].join(' | '));
+                if (i < 2) {
+                    console.log('---------');
                 }
             }
-        }
-        return true;
-    }
+        };
 
+        const getSelection = (rl) => {
+            rl.question(`Player ${currentPlayer}, enter the number of your column: `, (input) => {
+                const number = parseInt(input);
+                const [row, col] = findGridPosition(number);
+                if (row === 10 || col === 10 || typeof gameBoard[row][col] !== 'number') {
+                    console.log('Invalid selection! Enter again.');
+                    getSelection(rl); // Restart selection process
+                } else {
+                    updateGameBoard(row, col, rl);
+                }
+            });
+        };
 
+        const updateGameBoard = (row, col, rl) => {
+            updateGameStatus(row, col);
+            displayGameBoard();
+            if (checkWinner()) {
+                updateWinner();
+            } else if (isBoardFull()) {
+                determineIfGameIsDraw();
+            } else {
+                changeCurrentPlayer(rl);
+            }
+        };
 
-    function throwErrorForWrongSelection(row, col) {
-        console.log('invalid selection Enter again: ');
-        gameBoard[row][col] = currentPlayer;
-    }
+        const updateGameStatus = (row, col) => {
+            gameBoard[row][col] = currentPlayer;
+        };
 
-    function updateWinner() {
-        console.log(`Player ${currentPlayer} wins!`);
-        displayGameBoard();
-        rl.close();
+        const changeCurrentPlayer = (rl) => {
+            currentPlayer = (currentPlayer === 'X') ? 'O' : 'X';
+            getSelection(rl);
+        };
 
-    }
+        const determineIfGameIsDraw = () => {
+            console.log('It\'s a draw!');
+            displayGameBoard();
+            rl.close();
+        };
 
+        const isBoardFull = () => {
+            for (let i = 0; i < 3; i++) {
+                for (let j = 0; j < 3; j++) {
+                    if (typeof gameBoard[i][j] === 'number') {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        };
 
-    function checkRowWinner() {
-        for (let i = 0; i < 3; i++) {
-            if (
-                typeof gameBoard[i][0] !== 'number' &&
-                gameBoard[i][0] === gameBoard[i][1] && gameBoard[i][1] === gameBoard[i][2]
-            ) {
-                return true; // Found a winner in row i
+        const updateWinner = () => {
+            console.log(`Player ${currentPlayer} wins!`);
+            displayGameBoard();
+            rl.close();
+        };
+
+        const checkRowWinner = () => {
+            for (let i = 0; i < 3; i++) {
+                if (typeof gameBoard[i][0] !== 'number' &&
+                    gameBoard[i][0] === gameBoard[i][1] && gameBoard[i][1] === gameBoard[i][2]) {
+                    return true;
+                }
+                return false;
             }
         }
-        return false;
-    }
-
-    function checkColumnWinner() {
-        for (let i = 0; i < 3; i++) {
-            if (
-                typeof gameBoard[0][i] !== 'number' &&
-                gameBoard[0][i] === gameBoard[1][i] && gameBoard[1][i] === gameBoard[2][i]
-            ) {
-                return true; // Found a winner in column i
+        const checkColumnWinner = () => {
+            for (let i = 0; i < 3; i++) {
+                if (
+                    typeof gameBoard[0][i] !== 'number' &&
+                    gameBoard[0][i] === gameBoard[1][i] && gameBoard[1][i] === gameBoard[2][i]
+                ) {
+                    return true;
+                }
             }
-        }
-        return false;
-    }
+            return false;
+        };
 
-    function checkDiagonalWinner() {
-        if (
-            typeof gameBoard[0][0] !== 'number' &&
-            gameBoard[0][0] === gameBoard[1][1] && gameBoard[1][1] === gameBoard[2][2]
-        ) {
-            return true; // Found a winner in diagonal top-left to bottom-right
-        }
+        const checkDiagonalWinner = () => {
+            if (
+                typeof gameBoard[0][0] !== 'number' &&
+                gameBoard[0][0] === gameBoard[1][1] && gameBoard[1][1] === gameBoard[2][2]
+            ) {
+                return true;
+            }
 
-        if (
-            typeof gameBoard[0][2] !== 'number' &&
-            gameBoard[0][2] === gameBoard[1][1] && gameBoard[1][1] === gameBoard[2][0]
-        ) {
-            return true; // Found a winner in diagonal top-right to bottom-left
-        }
-        return false;
-    }
+            if (
+                typeof gameBoard[0][2] !== 'number' &&
+                gameBoard[0][2] === gameBoard[1][1] && gameBoard[1][1] === gameBoard[2][0]
+            ) {
+                return true;
+            }
+            return false;
+        };
 
-    function checkWinner() {
-        return checkRowWinner() || checkColumnWinner() || checkDiagonalWinner();
-    }
+        const checkWinner = () => {
+            return checkRowWinner() || checkColumnWinner() || checkDiagonalWinner();
+        };
 
+        const findGridPosition = (input) => {
+            if (input < 1 || input > 9) {
+                return [10, 10]; 
+            }
+            const row = Math.floor((input - 1) / 3);
+            const col = (input - 1) % 3;
+        
+            return [row, col];
 
-    return {
-        displayGameBoard,
-        getSelection,
-    }
+        };
 
+        return { displayGameBoard, getSelection };
+    };
 
-}();
+    return { createGame };
+})();
 
-function gameStart() {
-    gmae.displayGameBoard();
-    gmae.getSelection();
+const UIModule = (() => {
+    const readline = require('readline');
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
 
-}
+    const startGame = () => {
+        const game = GameModule.createGame(rl);
+        game.displayGameBoard();
+        game.getSelection(rl);
+    };
 
+    return { startGame };
+})();
 
-function findTheGridPosition(input) {
-    switch (input) {
-        case 1:
-            return [0, 0];
-        case 2:
-            return [0, 1];
-        case 3:
-            return [0, 2];
-        case 4:
-            return [1, 0];
-        case 5:
-            return [1, 1];
-        case 6:
-            return [1, 2];
-        case 7:
-            return [2, 0];
-        case 8:
-            return [2, 1];
-        case 9:
-            return [2, 2];
-
-        default:
-            return [10, 10];
-    }
-}
-
-
-gameStart();
+UIModule.startGame();
